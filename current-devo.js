1645c8dbfd3aff7eec1ba38cee21433bcfdd9f46
Justin Chang <justinjunochang@gmail.com>
2020-03-31 04:04:49 -0400

    jquery attempt 2

--- current-devo.js
+++ current-devo.js
@@ -1,10 +1,3 @@
-// window.addEventListener("load", function() {
-//   var extern = this.document.getElementsByTagName("link")[2].import;
-//   this.document
-//     .getElementById("devo-wrapper")
-//     .replaceWith(extern.getElementsByTagName("body")[0]);
-// });
-
 $(document).ready(function() {
   $("#devo-wrapper").load("devos/3-23-2020.html");
 });
