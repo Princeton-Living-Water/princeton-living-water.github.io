dcb02df1d20c50d61fad8461f2a5113bf82bf53c
Justin Chang <justinjunochang@gmail.com>
2020-03-31 04:01:53 -0400

    try jquery

--- current-devo.js
+++ current-devo.js
@@ -1,6 +1,10 @@
-window.addEventListener("load", function() {
-  var extern = this.document.getElementsByTagName("link")[2].import;
-  this.document
-    .getElementById("devo-wrapper")
-    .replaceWith(extern.getElementsByTagName("body")[0]);
+// window.addEventListener("load", function() {
+//   var extern = this.document.getElementsByTagName("link")[2].import;
+//   this.document
+//     .getElementById("devo-wrapper")
+//     .replaceWith(extern.getElementsByTagName("body")[0]);
+// });
+
+$(document).ready(function() {
+  $("#devo-wrapper").load("devos/3-23-2020.html");
 });
