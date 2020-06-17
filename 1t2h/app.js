16ab66b058026bb1419664491ce5c0776a54a91b
Joe Chen <joechen018@gmail.com>
2020-06-17 19:23:58 -0400

    styling changes to 1t2h

--- 1t2h/app.js
+++ 1t2h/app.js
@@ -5,8 +5,8 @@ var clicked=false;
 
 var question = {
     options: [
-      'The Son is always subordinate to the Father', 
-      'God is the Father, the Son, and the Holy Spirit but each are different persons', 
+      'The Son is always subordinate to the Father',
+      'God is the Father, the Son, and the Holy Spirit but each are different persons',
       'The Father, Son, and Holy Spirit are all the same and equally God'
     ],
     solution: 'option2',
@@ -23,50 +23,50 @@ function check(element){
   var remaining = question.options.slice();
   const index = remaining.indexOf(element.innerHTML.toString());
     if (id==question.solution){
-      element.style.backgroundColor = '#9acd32';
+      element.style.color = '#009900';
       element.className='finished';
       remaining.splice(index, 1);
       for (i = 0; i < remaining.length; i++){
         if(option1.innerHTML==remaining[i]){
-          option1.style.backgroundColor = '#ff4500';
+          option1.style.color = '#d73f3f';
           option1.className='finished';
         }
         if (option2.innerHTML==remaining[i]){
-          option2.style.backgroundColor = '#ff4500';
+          option2.style.color = '#d73f3f';
           option2.className='finished';
         }
         if (option3.innerHTML==remaining[i]){
-          option3.style.backgroundColor = '#ff4500';
+          option3.style.color = '#d73f3f';
           option3.className='finished';
         }
       }
     }
     else{
-      /* 
+      /*
       question.solution = 'option2'
       document.getElementById(question.solution)/objsoln = option2
       objsoln.innerHTML = 'Jesus is the Savior'
       */
-      element.style.backgroundColor = '#ff4500';
+      element.style.color = '#d73f3f';
       element.className='finished';
       remaining.splice(index, 1);
 
       var objsoln = document.getElementById(question.solution);
-      objsoln.style.backgroundColor = '#9acd32';
+      objsoln.style.color = '#009900';
       objsoln.className='finished';
       const sol = remaining.indexOf(objsoln.innerHTML);
       remaining.splice(sol, 1);
-      
+
       if(option1.innerHTML==remaining[0]){
-        option1.style.backgroundColor = '#ff4500';
+        option1.style.color = '#d73f3f';
         option1.className='finished';
       }
       if (option2.innerHTML==remaining[0]){
-        option2.style.backgroundColor = '#ff4500';
+        option2.style.color = '#d73f3f';
         option2.className='finished';
       }
       if (option3.innerHTML==remaining[0]){
-        option3.style.backgroundColor = '#ff4500';
+        option3.style.color = '#d73f3f';
         option3.className='finished';
       }
     }
@@ -77,4 +77,6 @@ function button(element){
   check(element);
   var x = document.getElementById('explanation');
   x.style.display = 'block';
+  var line = document.getElementById('line');
+  line.style.display = 'block';
 }
