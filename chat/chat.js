5c33376cd2731850b39377458bc0dcfc90971164
Joe Chen <joechen018@gmail.com>
2020-06-18 18:51:43 -0400

    cl

--- chat/chat.js
+++ chat/chat.js
@@ -34,7 +34,7 @@ function login() {
         if (response.data.status == "success") {
           document.cookie = "username=" + user_input;
           document.cookie = "token=" + response.data.token; 
-          if (response.data.admin === True)
+          if (response.data.admin === true)
             window.location.href = "./admin";
           else
             window.location.href = "./chat";
