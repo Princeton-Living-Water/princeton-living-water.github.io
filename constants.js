67ea79e60b93a46a9785626f3689d074c812f1e6
Joe Chen <joechen018@gmail.com>
2020-11-02 03:12:43 -0500

    url change

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
 const constants = {
-  "API_URL": "http://localhost:5000/",
-  "SOCKET_URL": "http://localhost:8000/",
-  "URL": "http://localhost:8080",
-  // "URL": "https://princetonlivingwater.org/",
-  // "API_URL" : "https://hungry-beaver-api.herokuapp.com/",
-  // "SOCKET_URL" : "https://hungry-beaver.herokuapp.com/"
+  // "API_URL": "http://localhost:5000/",
+  // "SOCKET_URL": "http://localhost:8000/",
+  // "URL": "http://localhost:8080",
+  "URL": "https://princetonlivingwater.org/",
+  "API_URL" : "https://hungry-beaver-api.herokuapp.com/",
+  "SOCKET_URL" : "https://hungry-beaver.herokuapp.com/"
 };
 
 export default constants;
