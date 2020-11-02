1e6a169bf38f8f13ef730587442d505a977ef5d2
Justin Chang <justinjunochang@gmail.com>
2020-11-02 00:08:23 -0500

    migrate to main site

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
 const constants = {
   // "API_URL": "http://localhost:5000/",
   // "SOCKET_URL": "http://localhost:8000/",
-  "URL": "http://localhost:8000",
+  // "URL": "http://localhost:8080",
+  "URL": "https://princetonlivingwater.org/",
   "API_URL" : "https://hungry-beaver-api.herokuapp.com/",
   "SOCKET_URL" : "https://hungry-beaver.herokuapp.com/"
 };
