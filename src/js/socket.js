75c3cea55bf3628f0bab619b6761427dadd5648d
Justin Chang <justinjunochang@gmail.com>
2020-11-16 01:13:54 -0500

    remove redirect on socket error

--- src/js/socket.js
+++ src/js/socket.js
@@ -24,7 +24,7 @@ const connectSocket = ({ name, token, room, setMessages, setNumMessages }) => {
 
   socket.on("error", (error) => {
     console.log(error);
-    navigate("/chat/login");
+    // navigate("/chat/login");
   });
 
   socket.on("unauthenticated", () => {
